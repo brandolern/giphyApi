941f652ec03dd3ec8c6006a620547b3f1f386fe9
brandolern <46857695+brandolern@users.noreply.github.com>
2019-04-07 21:07:51 -0600

    Added clear button and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
 
+    var characters = ["Batman", "Peter Griffin", "Nicholas Cage", "Christopher Walken", "Jason Statham", "Cartman", "Cat Woman", "Homer Simpson", "Slenderman", "Mario", "Sonic", "Link"];
+
+    for (i = 0; i < characters.length; i++) {
+        var button = $("<button>");
+        button.text(characters[i]);
+        button.addClass("btn btn-dark text-white gif-buttons");
+        button.attr("id", characters[i]);
+
+        $(".buttons").append(button);
+    };
+
     $(document).on("click", ".gif-buttons", function () {
         var search = ($(this).attr("id"));
-        var limit = 10;
+        var limit = 9;
         var rating = "R";
-        var url = `https://api.giphy.com/v1/gifs/search?api_key=66fQ1GJZy1MP8jyEJ6KJk1a6GkTWHpcX&q=${search}&limit=${limit}&offset=0&rating=${rating}&lang=en`
+        var url = `https://api.giphy.com/v1/gifs/search?api_key=66fQ1GJZy1MP8jyEJ6KJk1a6GkTWHpcX&q=${search}&limit=${limit}&offset=0&rating=${rating}&lang=en`;
 
         $.ajax({
             url: url,
@@ -16,6 +27,9 @@ $(document).ready(function () {
 
                 var gifUrl = response[i].images.fixed_height_still.url;
 
+                var newDiv = $("<div>");
+                newDiv.addClass("gif-div");
+
                 var p = $("<p>");
                 p.text("Rating: " + response[i].rating);
 
@@ -24,10 +38,10 @@ $(document).ready(function () {
                 createGif.attr("alt", search);
                 createGif.attr("data-value", i);
                 createGif.addClass("gifs");
-                $(".gif-container").prepend(createGif);
-                $(".gif-container").prepend(p);
 
-
+                $(newDiv).append(p);
+                $(newDiv).append(createGif);
+                $(".gif-container").prepend(newDiv);
             }
 
             var isMoving = false;
@@ -54,9 +68,15 @@ $(document).ready(function () {
 
         var button = $("<button>");
         button.text(searchInput);
-        button.addClass("btn btn-info gif-buttons");
+        button.addClass("btn btn-dark text-white gif-buttons");
         button.attr("id", searchInput);
 
         $(".buttons").append(button);
+        characters.push(searchInput);
+
     });
+    $(".clear").on("click", function () {
+        $(".gif-container").empty();
+    })
+
 });
